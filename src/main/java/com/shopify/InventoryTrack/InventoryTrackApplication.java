8cb92c81b1c025402d7f366071546dcc361f47ad
tom_glb <testtest_glb@gmail.com>
2022-05-21 11:53:48 -0400

    initital commit

---
+++ src/main/java/com/shopify/InventoryTrack/InventoryTrackApplication.java
@@ -0,0 +1,13 @@
+package com.shopify.InventoryTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTrackApplication.class, args);
+	}
+
+}
